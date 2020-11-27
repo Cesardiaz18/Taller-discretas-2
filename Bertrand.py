666909d064e5bd625b05f9e5dfbf818ceaa27a4e
Cesardiaz18 <71946251+Cesardiaz18@users.noreply.github.com>
2020-11-26 20:19:24 -0500

    Bertrand

---
+++ Bertrand.py
@@ -0,0 +1,16 @@
+
+import math
+def Bertrand_postulate ( n ) :
+  for i in range ( n , n*2 + 1): 
+    if ( i == 1 ):
+      continue 
+    prime = True
+    for d in range ( 2 , int(math.sqrt ( i )) + 1 ):
+      if ( i % d == 0 ):
+        prime = False
+    if ( prime == True ) : 
+      return i
+  return -1 ; 
+n = int ( input ( "Ingrese numero: " ) ) 
+print ( Bertrand_postulate( n ) )
+
