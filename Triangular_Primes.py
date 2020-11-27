fa0868d87696de4bb8128178dae2f6294e37b8b7
Cesardiaz18 <71946251+Cesardiaz18@users.noreply.github.com>
2020-11-26 20:17:53 -0500

    Update Triangular_Primes.py

--- Triangular_Primes.py
+++ Triangular_Primes.py
@@ -5,7 +5,7 @@ resp = True
 for i in range ( 3 , 101 ): 
     triangular = int(i * ( i + 1 ) / 2) 
     prime = True
-    for d in range ( 2 , int(math.sqrt(triangular)) + 1):
+    for d in range ( 2 , int(math.sqrt(triangular)) + 2):
       if ( triangular % d == 0 ):
         prime = False 
     if ( prime == True ):
