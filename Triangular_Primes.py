38d03b7c839b81c324c091fce515da60ed1683ae
Cesardiaz18 <71946251+Cesardiaz18@users.noreply.github.com>
2020-11-26 20:10:47 -0500

    Add files via upload

---
+++ Triangular_Primes.py
@@ -0,0 +1,17 @@
+
+
+import math
+resp = True
+for i in range ( 3 , 101 ): 
+    triangular = int(i * ( i + 1 ) / 2) 
+    prime = True
+    for d in range ( 2 , int(math.sqrt(triangular)) + 1):
+      if ( triangular % d == 0 ):
+        prime = False 
+    if ( prime == True ):
+      resp = False
+if ( resp == True ):
+  print ( "Afirmacion correcta, los numeros triangulares de 3 a 100 no son primos")
+else:
+  print ( "Afirmacion falsa")
+
